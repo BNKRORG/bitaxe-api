02b1febd68daaf44cdbbe5144f262cefa1893e55
Yuki Kishimoto <yukikishimoto@protonmail.com>
2025-11-26 12:36:24 +0100

    Add prelude module
    Signed-off-by: Yuki Kishimoto <yukikishimoto@protonmail.com>

---
+++ src/prelude.rs
@@ -0,0 +1,12 @@
+//! Prelude
+
+#![allow(unused_imports)]
+#![allow(unknown_lints)]
+#![allow(ambiguous_glob_reexports)]
+#![doc(hidden)]
+
+pub use ::url::*;
+
+pub use crate::client::{self, *};
+pub use crate::error::{self, *};
+pub use crate::response::{self, *};
